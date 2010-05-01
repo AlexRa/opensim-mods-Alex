e277dfcd53a4f14e30b0a0dba91b7f7bc4d8958b
AlexRa <info@adeptsql.com>
2010-05-01 18:22:57 +0300

    Testing the new migration code (not finished)

---
+++ OpenSim/Data/Tests/BasicMigrationTest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using NUnit.Framework.SyntaxHelpers;
+
+namespace OpenSim.Data.Tests
+{
+    [TestFixture]
+    class BasicMigrationTests
+    {
+        [Test]
+        public void 
+    }
+}
