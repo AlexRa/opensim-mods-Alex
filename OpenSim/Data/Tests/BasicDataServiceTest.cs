87b71a1b5469451d80411ca1cda893084c4c33da
AlexRa <info@adeptsql.com>
2010-05-06 22:07:28 +0300

    Corrections to BasicDataServiceTest - now with SQLite support
    Added platform check and creation/deletion of temp DB file
    (from SQLiteAssetTest), so none of the SQLite-specific test files
    are gonna be needed!

--- OpenSim/Data/Tests/BasicDataServiceTest.cs
+++ OpenSim/Data/Tests/BasicDataServiceTest.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using log4net.Config;
 using NUnit.Framework;
@@ -23,6 +24,7 @@ namespace OpenSim.Data.Tests
     {
         protected string m_connStr;
         private TService m_service;
+        private string m_file;
 
         // TODO: Is this in the right place here? 
         protected static readonly ILog m_log = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
@@ -52,6 +54,28 @@ namespace OpenSim.Data.Tests
         [TestFixtureSetUp]
         public void Init()
         {
+            // Sorry, some SQLite-specific stuff goes here (not a big deal, as its just some file ops)
+            if (typeof(TConn).Name.StartsWith("Sqlite"))
+            {
+                // SQLite doesn't work on power or z linux
+                if (Directory.Exists("/proc/ppc64") || Directory.Exists("/proc/dasd"))
+                    Assert.Ignore();
+
+                // for SQLite, if no explicit conn string is specified, use a temp file
+                if (String.IsNullOrEmpty(m_connStr))
+                {
+                    m_file = Path.GetTempFileName() + ".db";
+                    m_connStr = "URI=file:" + m_file + ",version=3";
+                }
+            }
+
+            if (String.IsNullOrEmpty(m_connStr))
+            {
+                string msg = String.Format("Connection string for {0} is not defined, ignoring tests", typeof(TConn).Name);
+                m_log.Error(msg);
+                Assert.Ignore(msg);
+            }
+
             // If we manage to connect to the database with the user
             // and password above it is our test database, and run
             // these tests.  If anything goes wrong, ignore these
@@ -77,6 +101,9 @@ namespace OpenSim.Data.Tests
                     ((IDisposable)m_service).Dispose();
                 m_service = null;
             }
+
+            if( !String.IsNullOrEmpty(m_file) && File.Exists(m_file) )
+                File.Delete(m_file);
         }
 
         protected virtual DbConnection Connect()
