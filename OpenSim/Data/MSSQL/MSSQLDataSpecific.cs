af7b48bb1a63a962f1596cde102143e39f983919
AlexRa <info@adeptsql.com>
2010-05-11 14:57:06 +0300

    Added "DBMS-specific" classes for MySQL/MS SQL/SQLite
    Each of these classes is specific to the DBMS, but not to a
    data store.
    
    Status:
    
    SQLite - NOT finished and NOT tested, although compiles OK
    MS SQL - tested, works fine,
    MySQL  - tested and works, Prepare() doesn't work for some reason
             (no errors, just does nothing)

---
+++ OpenSim/Data/MSSQL/MSSQLDataSpecific.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Data.Common;
+using System.Data;
+using System.Data.SqlClient;
+using OpenMetaverse;
+
+namespace OpenSim.Data.MSSQL
+{
+    public class MSSQLDataSpecific : DataSpecificBase
+    {
+        // TODO: redefine any data conversions that need be MS SQL-specific 
+        // (code from MSSQLManager would go here)
+
+        public override string DBName()
+        {
+            return "MS SQL";
+        }
+
+        protected override DbParameter MakeParam(DbCommand cmd, string sName, Type type, DataRow sch_row)
+        {
+            SqlParameter par = new SqlParameter();
+            par.ParameterName = sName;
+            par.Direction = ParameterDirection.Input;
+
+            // We'll have the 'sch_row' only when the parameter name matches one of the table columns.
+            // Otherwise (e.g. in a "limit @start, @count" clause) we have to use the type type 
+            if (sch_row != null)
+            {
+                par.SqlDbType = (SqlDbType)Convert.ToInt32(sch_row["ProviderType"]);
+                par.Size = Convert.ToInt32(sch_row["ColumnSize"]);
+
+                int x = Convert.ToInt32(sch_row["NumericPrecision"]);
+                if (x >= 0 && x <= 38)
+                    par.Precision = (byte)x;
+
+                x = Convert.ToInt32(sch_row["NumericScale"]);
+                if (x >= 0 && x <= 38)
+                    par.Scale = Convert.ToByte(sch_row["NumericScale"]);
+            }
+            else
+            {
+                if (type != null)   // no idea what the parameter is - really should never happen!
+                    par.SqlDbType = DbtypeFromType(type);
+                else
+                    par.DbType = DbType.Object;
+            }
+
+            return par;
+        }
+
+        /// <summary>
+        /// Type conversion to a SQLDbType functions
+        /// </summary>
+        /// <param name="type"></param>
+        /// <returns></returns>
+        internal SqlDbType DbtypeFromType(Type type)
+        {
+            if (type == typeof(string))
+            {
+                return SqlDbType.VarChar;
+            }
+            if (type == typeof(double))
+            {
+                return SqlDbType.Float;
+            }
+            if (type == typeof(Single))
+            {
+                return SqlDbType.Float;
+            }
+            if (type == typeof(int))
+            {
+                return SqlDbType.Int;
+            }
+            if (type == typeof(bool))
+            {
+                return SqlDbType.Bit;
+            }
+            if (type == typeof(UUID))
+            {
+                return SqlDbType.UniqueIdentifier;
+            }
+            if (type == typeof(sbyte))
+            {
+                return SqlDbType.Int;
+            }
+            if (type == typeof(Byte[]))
+            {
+                return SqlDbType.Image;
+            }
+            if (type == typeof(uint) || type == typeof(ushort))
+            {
+                return SqlDbType.Int;
+            }
+            if (type == typeof(ulong))
+            {
+                return SqlDbType.BigInt;
+            }
+            return SqlDbType.VarChar;
+        }
+
+        public override object UuidToDb(string uuid)
+        {
+            return new Guid(uuid);
+        }
+
+        /// <summary>Convert an UUID to whatever format is actually used in the database 
+        /// to keep UUIDs.  The result of this conversion is what you should supply as Cmd parameters.
+        /// </summary>
+        /// <param name="uuid">The UUID to be converted</param>
+        /// <returns></returns>
+        public override object UuidToDb(UUID uuid)
+        {
+            return uuid.Guid;
+        }
+
+        public override object BoolToDb(bool b)
+        {
+            return b ? 1 : 0;
+        }
+
+        public override bool DbToBool(object o)
+        {
+            return Convert.ToInt32(o) != 0;
+        }
+
+    }
+}
---
+++ OpenSim/Data/MySQL/MySqlDataSpecific.cs
@@ -0,0 +1,161 @@
+﻿/*
+ * Copyright (c) Contributors, http://opensimulator.org/
+ * See CONTRIBUTORS.TXT for a full list of copyright holders.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of the OpenSimulator Project nor the
+ *       names of its contributors may be used to endorse or promote products
+ *       derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE DEVELOPERS ``AS IS'' AND ANY
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE CONTRIBUTORS BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Data;
+using System.Data.Common;
+using MySql.Data.MySqlClient;
+using MySql.Data.Types;
+using OpenMetaverse;
+
+namespace OpenSim.Data.MySQL
+{
+    public class MySqlDataSpecific : DataSpecificBase
+    {
+        // TODO: redefine any data conversions that need be MySql-specific 
+        // (ok as is so far)
+
+        public override string DBName()
+        {
+            return "MySQL";
+        }
+
+        protected override DbParameter MakeParam(DbCommand cmd, string sName, Type type, DataRow sch_row)
+        {
+            MySqlParameter par = new MySqlParameter();
+            par.ParameterName = "@" + sName;
+            par.Direction = ParameterDirection.Input;
+
+            // We'll have the 'sch_row' only when the parameter name matches one of the table columns.
+            // Otherwise (e.g. in a "limit @start, @count" clause) we have to use the type type 
+            if (sch_row != null)
+            {
+                par.MySqlDbType = (MySqlDbType)Convert.ToInt32(sch_row["ProviderType"]);
+                par.Size = Convert.ToInt32(sch_row["ColumnSize"]);
+
+                int x = Convert.ToInt32(sch_row["NumericPrecision"]);
+                if (x >= 0 && x <= 38)
+                    par.Precision = (byte)x;
+
+                x = Convert.ToInt32(sch_row["NumericScale"]);
+                if (x >= 0 && x <= 38)
+                    par.Scale = Convert.ToByte(sch_row["NumericScale"]);
+            }
+            else
+            {
+                if (type != null)   // no idea what the parameter is - really should never happen!
+                    par.MySqlDbType = MySqlDbtypeFromType(type);
+                else
+                    par.DbType = DbType.Object;
+            }
+
+            return par;
+        }
+
+
+        // NOTE: MySql driver doesn't correctly map MySqlDbType to DbType, so we must have the override:
+
+        public override void SetParamValue(DbParameter par, object value)
+        {
+            if (value is UUID)
+            {
+                switch (((MySqlParameter)par).MySqlDbType)
+                {
+                    case MySqlDbType.Guid:
+                        par.Value = ((UUID)value).Guid;
+                        return;
+                    case MySqlDbType.Binary:
+                        par.Value = ((UUID)value).GetBytes();
+                        return;
+                    case MySqlDbType.String:
+                    case MySqlDbType.VarString:
+                    case MySqlDbType.VarChar:
+                        par.Value = ((UUID)value).ToString();
+                        return;
+                    default:
+                        par.Value = value;
+                        return;
+                }
+            }
+            par.Value = value;
+        }
+        internal MySqlDbType MySqlDbtypeFromType(Type type)
+        {
+            if (type == typeof(string))
+            {
+                return MySqlDbType.VarChar;
+            }
+            if (type == typeof(double))
+            {
+                return MySqlDbType.Float;
+            }
+            if (type == typeof(Single))
+            {
+                return MySqlDbType.Float;
+            }
+            if (type == typeof(int))
+            {
+                return MySqlDbType.Int32;
+            }
+            if (type == typeof(bool))
+            {
+                return MySqlDbType.Bit;
+            }
+            if (type == typeof(UUID))
+            {
+                return MySqlDbType.Guid;
+            }
+            if (type == typeof(sbyte))
+            {
+                return MySqlDbType.Int16;
+            }
+            if (type == typeof(Byte[]))
+            {
+                return MySqlDbType.Blob;
+            }
+            if (type == typeof(uint) || type == typeof(ushort))
+            {
+                return MySqlDbType.Int32;
+            }
+            if (type == typeof(ulong))
+            {
+                return MySqlDbType.Int64;
+            }
+            return MySqlDbType.VarChar;
+        }
+
+        /// <summary>This overridable returns a Migration object. For MySql that would be
+        /// a derived class, for other DBs this one would do.
+        /// </summary>
+        public override Migration GetMigration()
+        {
+            return new MySqlMigration();
+        }
+    }
+}
